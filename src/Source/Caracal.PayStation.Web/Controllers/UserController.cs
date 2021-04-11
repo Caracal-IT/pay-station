cc8dcb3b770260de07f58e09353406108b6937dd
Ettiene Mare <ettienemare@gmail.com>
2021-04-11 13:23:07 +0200

    Link web and api

--- src/Source/Caracal.PayStation.Web/Controllers/UserController.cs
+++ src/Source/Caracal.PayStation.Web/Controllers/UserController.cs
@@ -1,15 +1,33 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace Caracal.PayStation.Web.Controllers {
     [ApiController]
     [Route("[controller]")]
     public class UserController: ControllerBase {
+        static HttpClient client = new HttpClient();
+        
         [HttpPost("login")]
-        public ActionResult<LoginResponse> Login([FromBody] LoginRequest request) => 
-            Ok(new LoginResponse($"Welcome <em style='color:hotpink'>{request.Username}</em>"));
+        public async Task<ActionResult<LoginResponse>> Login([FromBody] LoginRequest request) {
+            var reason = "Login Failed";
+            HttpResponseMessage response = await client.PostAsJsonAsync("https://localhost:5001/users/Login", request);
+          //  response.EnsureSuccessStatusCode();
+          if (response.IsSuccessStatusCode) {
+              var ctx = await response.Content.ReadFromJsonAsync<UserContext>();
+              reason = ctx?.UserId.ToString();
+          }
+
+          return Ok(new LoginResponse($"Welcome <em style='color:hotpink'>{reason}</em>"));
+        }
     }
 
-    public record LoginRequest(string Username, string Password);
+    public record LoginRequest(string Username, string Password, long TenantId = 207);
 
     public record LoginResponse(string WelcomeMessage);
+
+    public record UserContext(int UserId, string Token);
 }
