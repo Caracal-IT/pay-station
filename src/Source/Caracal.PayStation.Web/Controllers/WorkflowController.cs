d3392dab30ddafa28278e677bc0f5e888d0b85ad
Ettiene Mare <ettienemare@gmail.com>
2021-04-16 20:36:00 +0200

    Fix caching

--- src/Source/Caracal.PayStation.Web/Controllers/WorkflowController.cs
+++ src/Source/Caracal.PayStation.Web/Controllers/WorkflowController.cs
@@ -8,21 +8,15 @@ namespace Caracal.PayStation.Web.Controllers {
     [ApiController]
     [Route("[controller]")]
     public class WorkflowController: ControllerBase {
-        private static readonly List<string> PublicWf = new() {"default", "login", "logout", "home"};
-        private bool IsLoggedIn => !string.IsNullOrWhiteSpace(Cookies.GetUserToken(Request));
         private readonly IWebHostEnvironment _environment;
         
         public WorkflowController(IWebHostEnvironment environment) => 
             _environment = environment;
 
         [HttpGet("process/{name}")]
-        [ResponseCache(Location = ResponseCacheLocation.Client, VaryByHeader = "X-UserToken", Duration = 1500000)]
+        [ResponseCache(Location = ResponseCacheLocation.Any, VaryByHeader = "X-Version", Duration = 604800)]
         public ActionResult GetProcess(string name) {
             var workflowDir =  _environment.ContentRootFileProvider.GetDirectoryContents("Workflow");
-
-            if (!IsLoggedIn && !PublicWf.Contains(name.ToLower())) 
-                name = "login";
-
             var process = workflowDir.FirstOrDefault(f => f.Name.Equals($"{name.ToLower()}.wf.json"));
 
             if (process == null)
