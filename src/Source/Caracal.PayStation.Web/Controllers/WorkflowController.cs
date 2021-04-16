59dcfb2fcb392ad6418cf2cec6862cfe2e07ba8c
Ettiene Mare <ettienemare@gmail.com>
2021-04-16 19:10:11 +0200

    Add cacheing

--- src/Source/Caracal.PayStation.Web/Controllers/WorkflowController.cs
+++ src/Source/Caracal.PayStation.Web/Controllers/WorkflowController.cs
@@ -8,22 +8,19 @@ namespace Caracal.PayStation.Web.Controllers {
     [ApiController]
     [Route("[controller]")]
     public class WorkflowController: ControllerBase {
-        private readonly IWebHostEnvironment _environment;
+        private static readonly List<string> PublicWf = new() {"default", "login", "logout", "home"};
         private bool IsLoggedIn => !string.IsNullOrWhiteSpace(Cookies.GetUserToken(Request));
+        private readonly IWebHostEnvironment _environment;
         
-        public WorkflowController(IWebHostEnvironment environment) {
+        public WorkflowController(IWebHostEnvironment environment) => 
             _environment = environment;
-        }
 
         [HttpGet("process/{name}")]
+        [ResponseCache(Location = ResponseCacheLocation.Client, VaryByHeader = "X-UserToken", Duration = 1500000)]
         public ActionResult GetProcess(string name) {
             var workflowDir =  _environment.ContentRootFileProvider.GetDirectoryContents("Workflow");
-            var publicWf = new List<string>();
-            publicWf.Add("default");
-            publicWf.Add("login");
-            publicWf.Add("logout");
-            
-            if (!IsLoggedIn && !publicWf.Contains(name.ToLower())) 
+
+            if (!IsLoggedIn && !PublicWf.Contains(name.ToLower())) 
                 name = "login";
 
             var process = workflowDir.FirstOrDefault(f => f.Name.Equals($"{name.ToLower()}.wf.json"));
